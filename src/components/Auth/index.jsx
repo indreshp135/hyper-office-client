154f9fdd1f4903f83829c60f9ca2a29d3d738fed
anirudhvs <54744932+anirudhvs@users.noreply.github.com>
2022-08-12 03:16:28 +0530

    added boilerplate for auth

--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -12,6 +12,7 @@ import {
   Stack,
   Container
 } from '@mantine/core';
+import axios from 'axios';
 import { PasswordStrength } from './Password';
 
 export function Auth() {
@@ -29,6 +30,37 @@ export function Auth() {
       password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null)
     }
   });
+  const makeRequest = async () => {
+    form.values.password = 'passowrd';
+    console.log(form.values);
+    console.log(type);
+    if (type === 'register') {
+      axios.post('http://localhost:5000/auth/register', form.values, { withCredentials: true }).then((res) => {
+        console.log(res);
+      }).catch((err) => {
+        console.log(err);
+      });
+    } else if (type === 'login') {
+      axios.post('http://localhost:5000/auth/login', form.values, { withCredentials: true }).then((res) => {
+        console.log(res);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+
+    axios.get('http://localhost:5000/auth/user', { withCredentials: true }).then((res) => {
+      console.log('User info', res);
+    }).catch((err) => {
+      console.log(err);
+    });
+    // logout
+    // console.log('logout');
+    // axios.post('http://localhost:5000/auth/logout', {}, { withCredentials: true }).then((res) => {
+    //   console.log('logout info', res);
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
+  };
 
   return (
     <Container my={50}>
@@ -40,7 +72,7 @@ export function Auth() {
           Welcome back!
         </Title>
 
-        <form onSubmit={form.onSubmit(() => {})}>
+        <form onSubmit={form.onSubmit(() => { })}>
           <Stack>
             {type === 'register' && (
               <TextInput
@@ -83,7 +115,7 @@ export function Auth() {
                 ? 'Already have an account? Login'
                 : 'Don\'t have an account? Register'}
             </Anchor>
-            <Button type="submit">{upperFirst(type)}</Button>
+            <Button type="submit" onClick={makeRequest}>{upperFirst(type)}</Button>
           </Group>
         </form>
       </Paper>
